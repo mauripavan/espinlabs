8fb1555f47100cb875c009a87931082b86d8d352
mauripavan <mauriciopavan60@gmail.com>
2023-05-01 22:06:42 -0300

    feat: funcion capicua

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,20 @@ function App() {
     },
   ];
 
+  //Hacer una función en Javascript que dado un string, devuelva si el mismo es capicúa. Hacer una llamada al mismo.
+  //<------------------------------------------------------------------------------>
+
+  const capicuaCheck = (s: string) => {
+    const array = s.split("").reverse();
+
+    const stringInvertido = array.join("");
+
+    return s === stringInvertido;
+  };
+
+  console.log('capicuaCheck ===>', capicuaCheck('reconocer'));
+  //<------------------------------------------------------------------------------>
+
   return (
     <div className="container">
       <Table data={data} />
